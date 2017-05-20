df434c96155b257045a0bdefa739c215cd2e73ec
cronokirby <cronokirby@gmail.com>
2017-05-21 01:02:08 +0200

    adds basic styling of the navbar

---
+++ web/static/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="navlinks">
+    <div v-for="path in navlinks" class="navlink" v-bind:id="path[0]">
+      <router-link v-bind:to="path[1]">{{path[0]}}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navlinks: [
+        ["Streams", "/"],
+        ["Races", "/races"],
+        ["Chat", "/chat"]
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#navlinks {
+  background-color: #18b0b5;
+  display: flex;
+  align-items: center;
+  padding-left: 8%;
+  box-shadow: 3px 3px 20px black;
+
+  a:link, a:visited {
+    color: #dce2e2;
+    display: block;
+    height: 100%;
+    line-height: 250%;
+    text-decoration: none;
+    text-align: center;
+  }
+
+  a:hover, a:active {
+    background-color: #1cb9be;
+    box-shadow: 0px 0px 20px #133231;
+  }
+
+  .navlink {
+    height: 40px;
+    flex-basis: 100px;
+    font-family: sans-serif;
+    font-weight: bold;
+  }
+}
+</style>
--- web/static/components/base.vue
+++ web/static/components/base.vue
@@ -1,53 +1,30 @@
 <template>
   <div class="base">
-    <div id="navlinks">
-      <div v-for="path in navlinks" class="navlink" v-bind:id="path[0]">
-        <router-link v-bind:to="path[1]">{{path[0]}}</router-link>
-      </div>
-    </div>
+    <nav-bar></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "./NavBar.vue"
+
 export default {
-  data() {
-    return {
-      navlinks: [
-        ["Races", "/races"],
-        ["Chat", "/chat"]
-      ]
-    }
+  components: {
+    NavBar
   }
 }
 </script>
 
 <style lang="scss">
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #3a3a49;
+}
+
 .base {
   width: 100%;
   position: absolute;
   top: 0;
   left: 0;
-}
-
-#navlinks {
-  background-color: #2b4b94;
-  display: flex;
-  align-items: center;
-  padding-left: 20px;
-
-  a:link, a:visited {
-    color: white;
-    display: block;
-    height: 100%;
-    text-decoration: none;
-    text-align: center;
-  }
-  a:hover, a:active {
-    background-color: blue;
-  }
-  .navlink {
-    height: 30px;
-    flex-basis: 100px;
-  }
 }
 </style>
