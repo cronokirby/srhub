51dfec92c5c4d32c369df790f61628af4af02840
cronokirby <cronokirby@gmail.com>
2017-05-25 18:45:08 +0200

    adds teh sign in component

---
+++ web/static/components/Overlay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="$root.overlay" class="overlay">
+    <button v-on:click="$root.overlay = false">turn me off</button>
+    <sign-in></sign-in>
+  </div>
+</template>
+
+<script>
+import SignIn from "./SignIn.vue"
+
+export default {
+  components: {
+    SignIn
+  }
+}
+</script>
+
+<style lang="scss">
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 10, 0.4);
+  z-index: 10;
+}
+</style>
---
+++ web/static/components/SignIn.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="sign-in">
+    <div class="user-inp">
+      Username<br/>
+      <input v-model="username"/>
+    </div>
+    <div class="user-inp">
+      Password<br/>
+      <input type="password" v-model="password"/>
+    </div>
+    <div id="login-btn">
+      <button class="loginbtn">Log In</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.sign-in {
+  position: absolute;
+  width: 240px;
+  top: 10%;
+  left: 0;
+  right: 0;
+  margin: auto auto;
+  border: 2px solid #2fc9cd;
+  box-shadow: 3px 3px 10px black;
+  z-index: 11;
+  padding: 10px;
+}
+
+.user-inp {
+  color: white;
+  $vertical: 15px;
+  margin: $vertical 0px $vertical 30px;
+
+  input {
+    margin-top: 10px;
+    padding-left: 10px;
+    height: 24px;
+    font-size: 14px;
+    font-weight: 550;
+    color: #232a2d;
+    border: solid 1px #dcdcdc;
+    transition: box-shadow 0.3s, border 0.3s;
+  }
+
+  input:focus {
+    outline: none;
+    border: solid 1px #707070;
+    box-shadow: 0px 0px 4px 1px #54d7da;
+  }
+}
+
+#login-btn {
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 10px;
+
+  button {
+    all: unset;
+    font-size: 15px;
+    font-weight: 500;
+    color: #d2e3e3;
+    background-color: #18b0b5;
+    height: 25px;
+    width: 60px;
+    box-shadow: 3px 3px 10px black;
+  }
+
+  button:active {
+    box-shadow: inset 0px 0px 5px #133231;
+  }
+}
+</style>
